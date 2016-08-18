fe5983a525b6d906d6f5478ecc509e56b08ad1b0
Maksim Klyushkov <klu4ik@trige.ru>
2016-08-18 12:05:54 +0300

    create reload_rtmp.js

---
+++ reload_rtmp.js
@@ -0,0 +1,30 @@
+var skip = 0;
+function dump(obj) {
+    var out = "";
+    if(obj && typeof(obj) == "object"){
+        for (var i in obj) {
+            out += i + ": " + obj[i] + "\n";
+                if(obj[i] == "playback_play"){
+                        if(window.skip == 2){
+                                window.skip = 0;
+                                var iframe = document.getElementById('camera');
+                                iframe.src = iframe.src;
+                                console.log("RELOAD");
+                                } else { window.skip += 1; }
+                        }
+        }
+    } else {
+        out = obj;
+    }
+    console.log(out);
+}
+function listener(event) {
+  dump(event.data);
+}
+
+if (window.addEventListener) {
+  window.addEventListener("message", listener);
+} else {
+  // IE8
+  window.attachEvent("onmessage", listener);
+}
